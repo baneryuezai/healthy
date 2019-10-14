a44cac7635faac98a2f66b86f62ca186071762ec
Administrator <fangq17@163.com>
2019-10-14 10:36:07 +0800

    增加redis缓存操作

--- healthy_common/src/main/java/com/IT/liuJia/constant/RedisConstant.java
+++ healthy_common/src/main/java/com/IT/liuJia/constant/RedisConstant.java
@@ -5,4 +5,8 @@ public interface RedisConstant {
      static final String SETMEAL_PIC_RESOURCES = "packagePicResources";
     //套餐图片保存在数据库中的图片名称
      static final String SETMEAL_PIC_DB_RESOURCES = "packagePicDbResources";
+    //套餐列表保存在redis中的名称
+    static final String SETMEAL_PACKAGELIST_RESOURCES = "packageList";
+    //套餐详情在redis中保存的名称
+    static final String SETMEAL_PACKAGE_RESOURCES = "package";
 }
